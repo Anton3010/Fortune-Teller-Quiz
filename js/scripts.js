e640173db7f8cbfce86354c15df3bde3dc5382c1
Anton Sharykin and Uriel Gonzalez <uriel@>
2019-10-29 16:42:16 -0700

    Add form and functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,30 @@
 $(document).ready(function() {
+  $("form#fortuneQuiz").submit(function(event) {
+    event.preventDefault();
+
+    $("#results").show();
+    $("input:checkbox[name=good-fortune]:checked").each(function() {
+      // var goodFortuneResult = $(this).val();
+      // $('#results').append(goodFortuneResult + "<br>");
+      var inputs = $(this).val();
+      $('#results').append(inputs + "<br>");
+      $('#results').text("You're a lucky one!");
+
+    });
+
+    $("input:checkbox[name=bad-fortune]:checked").each(function() {
+      // var goodFortuneResult = $(this).val();
+      // $('#results').append(goodFortuneResult + "<br>");
+      var inputs2 = $(this).val();
+      $('#results').append(inputs2 + "<br>");
+      $('#results').text("You're screwed!");
 
 
+    });
+    // if ('#results').append(inputs + inputs2 + "<br>"){
+    //   $('#results').text("You are well balanced!");
+    // }
 
+    $('#fortuneQuiz').hide();
+  });
 });
